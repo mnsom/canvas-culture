5fdff9a6884213a102cce17c5c18c0f99028fb58
Lisa Takenouchi <rhrbanb@gmail.com>
2023-08-15 15:45:28 +0900

    swork in progress

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,4 +3,19 @@ class RequestsController < ApplicationController
     @requests = Request.all
   end
 
+  def create
+    raise
+    @request = Request.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+      if @bookmark.save
+        redirect_to list_path(@list)
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+  def request_params
+    params.require(:request).permit(:start_date, :end_date)
+  end
+
 end
