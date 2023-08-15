3fab897728124d6451e11dc0988ccd31dde8d61d
Mana Misawa <mnmisawa04@gmail.com>
2023-08-15 15:07:14 +0900

    fixed cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @paintings = Painting.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,11 @@ Request.create!(status: "accepted", start_date: Date.new(2023, 8, 29), end_date:
 
 # Mona Lisa
 # the japanase wave next to fujis
+<<<<<<< Updated upstream
 
+=======
+# "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+>>>>>>> Stashed changes
 # Monet Paiting
 
 # Frida Khalo
