ad770fb6f8055b624c0b27b01d89c387d840d4ba
Misako Moriyama <misa108d23106@yahoo.co.jp>
2023-08-15 12:18:34 +0900

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+misako = User.create!(email: "misako@me.com", password: "secret")
+lisa = User.create!(email: "lisa@me.com", password: "secret")
+
+Painting.create(title: "Mona Lisa (Da Vinci)", price: 100000, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", user: misako)
+Painting.create(title: "Woman with a Parasol (Monet)", price: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", user: lisa)
+Painting.create(title: "Starry Night (Vincent Van Gogh)", price: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", user: lisa)
+Painting.create(title: "The Great Wave off Kanagawa (Hokusai)", price: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", user: lisa)
+Painting.create(title: "The Great Wave off Kanagawa (Frida Khalo)", price: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", user: misako)
+p "#{Painting.count} Paintings are created"
+
+# List.create(name: "Drama")
+# List.create(name: "Sci-fi")
+# List.create(name: "Girl power")
+# p "#{List.count} Lists are created"
+
+
+# Mona Lisa
+# the japanase wave next to fujis
+
+# Monet Paiting
+
+# Frida Khalo
+
+# Da Vinci
