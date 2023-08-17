685a0dd7245731c4f11a10592868d2ab7412a722
Mana Misawa <mnmisawa04@gmail.com>
2023-08-17 12:31:10 +0900

    added an index action of owner

--- app/controllers/owner/requests_controller.rb
+++ app/controllers/owner/requests_controller.rb
@@ -1,4 +1,6 @@
 class Owner::RequestsController < ApplicationController
   def index
+    @user = current_user
+    @requests = @user.requests_as_owner
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # validates :name, presence: true
+  has_many :requests # requests as a client
+  has_many :paintings
+  has_many :requests_as_owner, through: :paintings, source: :requests
 end
