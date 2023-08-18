206381f40dd0617c2e08997c201bd49ca4685dac
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-08-18 16:21:38 +0900

    done adding validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,8 @@ class Request < ApplicationRecord
   belongs_to :painting
   belongs_to :user
   enum status: { rejected: "rejected", pending: "pending", accepted: "accepted" }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 
   def pending?
     status == 'pending'
